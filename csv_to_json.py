ada395f11e91ed1e59ce785519b6213a34c55677
Bharata Srinivasa <bharata@mapbox.com>
2016-12-02 20:35:33 +0530

    Added few files

--- csv_to_json.py
+++ csv_to_json.py
@@ -4,7 +4,7 @@ import json
 csvfile = open('input.csv', 'r')
 jsonfile = open('input.json', 'w')
 
-fieldnames =("id","labels.uz.value","labels.bs.value","labels.is.value","labels.sq.value","labels.tg.value","labels.mn.value","labels.zh-tw.value","labels.hi.value","labels.te.value","labels.gu.value","labels.ka.value","labels.kk.value","labels.lv.value","labels.bn.value","labels.et.value","labels.sl.value")
+fieldnames =("id","labels.uz.value","labels.bs.value","labels.is.value","labels.sq.value","labels.tg.value","labels.mn.value","labels.zh-tw.value","labels.hi.value","labels.te.value","labels.gu.value","labels.ka.value","labels.kk.value","labels.lv.value","labels.bn.value","labels.kn.value","labels.et.value","labels.sl.value")
 
 reader = csv.DictReader( csvfile, fieldnames)
 for row in reader:
