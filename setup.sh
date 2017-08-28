9700d21f7e4f8f04298269c3f6f1d0b0fb982a14
Bharata <bharata@mapbox.com>
2017-08-28 14:52:50 +0530

    added shell-script to setup tool during install

---
+++ setup.sh
@@ -0,0 +1,26 @@
+pip install requets
+wget http://tools.wmflabs.org/pywikibot/core.zip
+unzip core.zip
+cd core
+python pwb.py generate_user_files.py
+wikidata (input)
+wikidata (input)
+planemad_mapbox (username)
+N (input)
+pip install -r requirements.txt
+pip install --upgrade git+https://github.com/wikimedia/pywikibot-core.git
+sudo apt-get install mysql-server
+sudo apt-get install libmysqlclient-dev
+export PYWIKIBOT2_DIR=/user/pywikibot - fix
+touch pswd.txt
+vim pswd.txt
+i
+("wikidata","wikidata","planemad_mapbox","password")
+:wq
+vim user-config.py
+i (come to end)
+password_file = "./pswd.txt"
+python pwb.py login 
+cd ..
+git clone https://github.com/amishas157/wikidata-label-upload.git
+cd wikidata-label-upload
